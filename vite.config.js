f4d3dc9e4c5396caf083cdbb91d2429691fa6f42
Aditya Saini <102839228+adityasaini823@users.noreply.github.com>
2025-04-09 23:43:08 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss(),  ],
+  server: {
+    host: "0.0.0.0", // This ensures the server is available externally.
+    port: 5173, // Use the PORT environment variable (if provided) or default to 5173.
+  },
 })
